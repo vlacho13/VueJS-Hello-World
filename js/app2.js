18ed8c8c97e248106ff21156626ff29a6882f014
vlacho13 <striuncekf@hotmail.com>
2018-10-15 23:28:45 -0500

    Added part 4 - Directives

---
+++ js/app2.js
@@ -0,0 +1,31 @@
+//EXAMPLE OF DIRECTIVE
+Vue.directive("salut", {
+  bind: function(el, binding, vnode) {
+    el.value = binding.value
+    console.log("bind")
+  },
+  update: function(el, binding, vnode, oldvnode) {
+    console.log("update")
+  }
+})
+/*EXAMPLE OF DIRECTIVE(VARIABLE) -- Must be added inside Vue object
+let hello = function(el, binding, vnode) {
+  el.value = binding.value
+  console.log("bind")
+}
+*/
+
+let vm = new Vue({
+  el: "#app",
+  directives: {
+    hello
+  },
+  data: {
+    message : "",
+    number: "",
+    test: "Test..."
+  },
+  methods: {
+    
+  }
+});
