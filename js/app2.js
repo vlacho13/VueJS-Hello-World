fe106b46addcb64486357933ebae8626290e06ef
vlacho13 <striuncekf@hotmail.com>
2018-10-16 22:58:08 -0500

    Added Part 5 - filters

--- js/app2.js
+++ js/app2.js
@@ -1,28 +1,41 @@
-//EXAMPLE OF DIRECTIVE
+//EXAMPLE OF DIRECTIVES
 Vue.directive("salut", {
   bind: function(el, binding, vnode) {
     el.value = binding.value
     console.log("bind")
   },
   update: function(el, binding, vnode, oldvnode) {
-    console.log("update")
+    console.log("updated!")
   }
 })
-/*EXAMPLE OF DIRECTIVE(VARIABLE) -- Must be added inside Vue object
+//EXAMPLE OF DIRECTIVE(VARIABLE) -- Must be added inside Vue object
 let hello = function(el, binding, vnode) {
   el.value = binding.value
   console.log("bind")
 }
-*/
+
+//EXAMPLES OF FILTERS
+Vue.filter("capitalize", function(value) {
+  return value.toUpperCase();
+})
+let reverse = function(value) {
+  return value.split(" ").reverse().join(" ");
+}
+
 let vm = new Vue({
   el: "#app",
-  /*directives: {
-    hello: 
-  },*/
+  directives: {
+    hello
+  },
   data: {
     message : "",
+    message2: "this is capitalized filter!",
+    message3: "this is a capitalized reversed filter!",
     number: "",
     test: "Test..."
+  },
+  filters: {
+    reverse
   },
   methods: {
     
